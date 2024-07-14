350b96a8eba9f0e8e4cf1f1aa05c5d93d6f4f6ec
pitanihamsaraj <pitanihamsaraj@gmail.com>
2024-07-14 21:36:35 +0530

    backend

--- server.js
+++ server.js
@@ -21,3 +21,5 @@ app.use('/api/member', memberRoutes);
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
+
+module.exports = app
