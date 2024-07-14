306fa201d559635619cd761a13c46e3bd4eb66c6
pitanihamsaraj <pitanihamsaraj@gmail.com>
2024-07-14 21:17:27 +0530

    first commit

--- config/keys.js
+++ config/keys.js
@@ -6,12 +6,4 @@ module.exports = {
     connectURL:
       "mongodb+srv://pitanihamsaraj:kJNhiY3PXedIjHTO@cluster0.gmejwfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   },
-  jwt: {
-    secret: process.env.JWT_SECRET,
-    tokenExp: "1d",
-  },
-  mailer: {
-    noReplyEmailId: "noreply@roombuddy.in",
-    apiKey: process.env.SENDGRID_API_KEY,
-  },
 };
