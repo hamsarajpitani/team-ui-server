32b139c50601ec0e2d3c3bc8240a0c3f1e036544
pitanihamsaraj <pitanihamsaraj@gmail.com>
2024-07-15 23:11:07 +0530

    cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { connectDB } = require("./config/db");
 //to recognize JSOn
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 //* DATABASE
 connectDB();
